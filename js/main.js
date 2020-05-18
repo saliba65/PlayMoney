bb5337a46446ea3a7c991e276f1928d1727fad2c
saliba65 <lucassaliba10@gmail.com>
2020-05-18 14:58:25 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+//Select element function
+
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function () {
+    body.classList.toggle('open');
+});
+
+//Scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+$(function(){
+    $('.baixe_agora, .close').on('click', function(){
+	    $('.google_apple_store').toggleClass('show');
+	});
+
+	$('.nav-item').on('click', function(){
+		$("body").removeClass("open");
+	});
+})
